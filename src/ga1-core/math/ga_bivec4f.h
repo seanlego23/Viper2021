3bac54d8d9eb6a779279249d6483ac83632b1744
seanlego23 <seanlego23@gmail.com>
2022-04-17 16:18:15 -0400

    Add variables

--- src/ga1-core/math/ga_bivec4f.h
+++ src/ga1-core/math/ga_bivec4f.h
@@ -1,6 +1,20 @@
 #pragma once
 struct ga_bivec4f 
 {
+	union 
+	{
+		float i[6];
+		struct 
+		{
+			float xy;
+			float xz;
+			float xw;
+			float yz;
+			float yw;
+			float zw;
+		};
+	};
+
 
 };
 
